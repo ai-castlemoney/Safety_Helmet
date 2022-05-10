a1acba3386f3bccfbd4666da312eb230a8677832
NAMCHEOL HER <namcheolher@NAMCHEOLui-MacBookAir.local>
2022-05-10 19:45:07 +0900

    txt to df code ongoing

--- code_txt_to_df/txt_to_df.py
+++ code_txt_to_df/txt_to_df.py
@@ -24,85 +24,253 @@ def load_txt(txt_path):
     doc_code = txt_path.split('.')[-2].split('/')[-1]
     with open(txt_path,'r') as txt_data:
         txt = txt_data.readlines()
-    txts = {doc_code:txt}
+    txts = {doc_code:txt} 
     return txts
+
 # 경로의 파일 모두 읽기
 def load_txts(read_dir):
-    
     dir_list = os.listdir(read_dir) # 읽기 경로의 파일들
     txts = {}
     for txt_name in dir_list:
-
         print(txt_name)
         doc_code = txt_name.split(".")[0]
         txt_path = read_dir +'/' + txt_name
-
         with open(txt_path,'r') as txt_data:
             txt = txt_data.readlines() # lines 는 각 문장이 리스트로 들어간다.
         txts[doc_code] = txt
     return txts
 
-# 각 문서 txt를 df에 추가
-def txt_to_df(doc_code, txt):
-    cols = ["doc_code", "t1", "t2", "t3", "c1", "c2", "c3", "c4", "title", "content", "image", "table", "appendix"]
-    #Create a DataFrame object
-    df = pd.DataFrame(columns = cols)
-    new_row = {
-        "doc_code" : doc_code,
-        "t1" : None,
-        "t2" : None,
-        "t3" : None,
-        "c1" : None,
-        "c2" : None,
-        "c3" : None,
-        "c4" : None,
-        "title" : None,
-        "content" : None,
-        "image" : None,
-        "table" : None,
-        "appendix" : None}
+def get_col_type(line_head, cols):
 
+    t1 = re.compile('\d+[.]') # title 1 : 1.
+    t2 = re.compile('\d+[.]\d+') # title 2 : 1.1
+    t3 = re.compile('\d+[.]\d+[.]\d+') # title 3 : 1.1.1
+    c1 = re.compile('[(]\d+[)]') # content 1 : (1)~
+    c2 = re.compile('[(][가-힣][)]') # content 2 : (가)~(힣)
+    c3 = re.compile('[①-⑳]') # content 3 : ①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮⑯⑰⑱⑲⑳
+    c4 = re.compile('[㉮-㉻]') # content 4  : ㉮㉯㉰㉱㉲㉳㉴㉵㉶㉷㉸㉹㉺㉻
+
+    if t3.match(line_head):
+        col_type = "t3"
+    elif t2.match(line_head):
+        col_type = "t2"
+    elif t1.match(line_head):
+        col_type = "t1"
+    elif c1.match(line_head):
+        col_type = "c1"
+    elif c2.match(line_head):
+        col_type = "c2"
+    elif c3.match(line_head):
+        col_type = "c3"
+    elif c4.match(line_head):
+        col_type = "c4"
+    elif line_head == '-':
+        col_type = '-'
+    elif line_head == '그림':
+        col_type = 'image'
+    elif line_head == '표':
+        col_type = "table"
+    elif line_head == '부록':
+        col_type = 'appendix'
+    elif line_head == '수식':
+        col_type = 'math_exp'
+    elif line_head == '붙임':
+        col_type = 'attachment'
+    elif line_head == '참고':
+        col_type = 'reference'
+    elif line_head == '별지_그림':
+        col_type = 'enclosure_image'
+    elif line_head == '별지':
+        col_type = 'enclosure'
+    elif line_head == '별표':
+        col_type = 'asterisk'
+    elif line_head == '부록_표':
+        col_type = 'appendix_table'
+    else:
+        col_type = 'content'
+
+    return col_type
+
+# 수식, 붙임, 참고, 별지_그림, 별지, 별표, 부록_표
+#     "math_exp",
+#     "attachment",
+#     "reference",
+#     "enclosure_image",
+#     "enclosure",
+#     "asterisk",
+#     'appendix_table']
+
+# 각 문서 txt를 df에 추가
+def txt_to_df(doc_code, txt, df, cols):
     # t1~3, c1~4 정규표현식
-    t1 = re.compile('^\d+[.] ')
-    t2 = re.compile('^\d+[.]\d+ ')
-    t3 = re.compile('^\d+[.]\d+[.]\d+ ')
-    c1 = re.compile('^[(]\d+[)] ')
-    c2 = re.compile('^[(][가-하][)] ')
-    c3 = re.compile('^[①-⑳] ')
-    c4 = re.compile('^[①-⑳] ')
+    new_row = { col_name : None for col_name in cols} # 추가할 행 초기화
+    new_row["doc_code"] = doc_code # 문서코드 행에 추가
 
     for line in txt: # 각 문장에 대해서
-        line_splitted = line.split()
-        col_type = line_splitted[0]
-        data = " ".join(line_splitted[1:])
 
-        print(col_type)
+        # 글머리, 내용을 분리
+        line_splitted = line.split()
+        line_head = line_splitted[0] # 글머리
+        data = " ".join(line_splitted[1:]) # 내용
+
+        # 글머리로 부터 col_type 얻기
+        col_type = get_col_type(line_head, cols)
+        print(line_head, col_type)
+        # col_type 이 t#, c# 이외인 경우
+        if col_type == "t1": # t1 의 경우
+            new_row["t1"] = line_head
+            new_row["t2"] = None
+            new_row["t3"] = None
+            new_row["c1"] = None
+            new_row["c2"] = None
+            new_row["c3"] = None
+            new_row["c4"] = None
+            new_row["title"] = data
+        elif col_type == "t2": # t2 의 경우
+            new_row["t2"] = line_head
+            new_row["t3"] = None
+            new_row["c1"] = None
+            new_row["c2"] = None
+            new_row["c3"] = None
+            new_row["c4"] = None
+            new_row["title"] = data
+        elif col_type == "t3":
+            new_row["t3"] = line_head
+            new_row["c1"] = None
+            new_row["c2"] = None
+            new_row["c3"] = None
+            new_row["c4"] = None
+            new_row["title"] = data
+        elif col_type == "c1":
+            new_row["c1"] = line_head
+            new_row["c2"] = None
+            new_row["c3"] = None
+            new_row["c4"] = None
+            new_row["content"] = data
+        elif col_type == "c2":
+            new_row["c2"] = line_head
+            new_row["c3"] = None
+            new_row["c4"] = None
+            new_row["content"] = data
+        elif col_type == "c3":
+            new_row["c3"] = line_head
+            new_row["c4"] = None
+            new_row["content"] = data
+        elif col_type == "c4":
+            new_row["c4"] = line_head
+            new_row["content"] = data
+        elif col_type == "-":
+            if pre_col_type == "t1":
+                new_row["t2"] = line_head
+                new_row["t3"] = None
+                new_row["c1"] = None
+                new_row["c2"] = None
+                new_row["c3"] = None
+                new_row["c4"] = None
+                new_row["title"] = data
+            elif pre_col_type == "t2":
+                new_row["t3"] = line_head
+                new_row["c1"] = None
+                new_row["c2"] = None
+                new_row["c3"] = None
+                new_row["c4"] = None
+                new_row["title"] = data
+            elif pre_col_type == "t3":
+                new_row["c1"] = line_head
+                new_row["c2"] = None
+                new_row["c3"] = None
+                new_row["c4"] = None
+                new_row["content"] = data
+            elif pre_col_type == "c1":
+                new_row["c2"] = line_head
+                new_row["c3"] = None
+                new_row["c4"] = None
+                new_row["content"] = data
+            elif pre_col_type == "c2":
+                new_row["c3"] = line_head
+                new_row["c4"] = None
+                new_row["content"] = data
+            elif pre_col_type == "c3":
+                new_row["c4"] = line_head
+                new_row["content"] = data
+            else:
+                pass
+        # col_type 이 그림 표 부록 등 이전행에 추가될 자료, 이전 행에 추가 후 다음 반복으로(continue)
+        ## 바로 내용이 나오는 content 도 마찬가지.
+        elif col_type in ["content","image","table","appendix","math_exp","attachment","reference","enclosure_image","enclosure","asterisk",'appendix_table']:
+             # df 마지막 행의 [col_type] = data
+            print(df.iloc[-1][col_type])
+            print('여기?')
+            if df.iloc[-1][col_type] != None : # 기존에 항목이 있으면 ", data"를 추가
+                df.iloc[-1][col_type] = df.iloc[-1][col_type] + ", " + data
+            else:
+                df.iloc[-1][col_type] = data
+            continue
+
+        else:
+            print(col_type, data, "어디에도 속하지 않습니다.")
+            pass
+
+        # new_row를 df 마지막에 추가
+        new_df = pd.DataFrame([new_row])
+        df = pd.concat([df,new_df],ignore_index=True)
+        #df = df.append(new_row, ignore_index=True)
+
+
+        # t#, c# 을 제외하고 new_row 초기화
+        for col_name in ["title","content","image","table","appendix","math_exp","attachment","reference","enclosure_image","enclosure","asterisk",'appendix_table']:
+            new_row[col_name] = None
+            
+        #print(line_head, col_type)
+        # pre_col_type 복제
+        pre_col_type = col_type
     print(df)
     return df
 
+#----------------- MAIN ---------------------#  
+
+# df 초기화, 사용할 컬럼명 지정 
+# 차례로 문서코드, 제목1~3, 내용1~4, 제목, 내용, 그림, 표, 부록, 수식, 붙임, 참고, 별지_그림, 별지, 별표, 부록_표
+cols = [
+    "doc_code",
+    "t1",
+    "t2",
+    "t3",
+    "c1",
+    "c2",
+    "c3",
+    "c4",
+    "title",
+    "content",
+    "image",
+    "table",
+    "appendix",
+    "math_exp",
+    "attachment",
+    "reference",
+    "enclosure_image",
+    "enclosure",
+    "asterisk",
+    'appendix_table']
+
+#Create a DataFrame object
+df = pd.DataFrame(columns = cols)
+new_row = { col_name : None for col_name in cols} # 추가할 행 초기화
+#df = df.append(new_row, ignore_index=True)
+new_df = pd.DataFrame([new_row])
+df = pd.concat([df,new_df])
+print("concat done")
+
 txt_path = "txt_preprocessing_r2/C-01-2011.txt"
 txts = load_txt(txt_path)
+print(txts)
 
-# df 초기화
-cols = ["doc_code", "t1", "t2", "t3", "c1", "c2", "c3", "c4", "title", "content", "image", "table", "appendix"]
-    #Create a DataFrame object
-    df = pd.DataFrame(columns = cols)
-    new_row = {
-        "doc_code" : doc_code,
-        "t1" : None,
-        "t2" : None,
-        "t3" : None,
-        "c1" : None,
-        "c2" : None,
-        "c3" : None,
-        "c4" : None,
-        "title" : None,
-        "content" : None,
-        "image" : None,
-        "table" : None,
-        "appendix" : None}
+save_dir = 'hackerthon/저장 경로'
 for doc_code, txt in txts.items():
-    txt_to_df(doc_code, txt)
+    df = txt_to_df(doc_code, txt, df, cols)
+
+    df.to_csv('/Users/namcheolher/aiffel/Safety_Helmet/code_txt_to_df'+'/'+doc_code+'.csv', index=False)
+
 
 
 # Get file names in dir
